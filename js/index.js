1f782e1e5b4abe3ace2cbe3369cff3f9c40a461a
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-09-26 18:00:11 +0200

    add function

--- js/index.js
+++ js/index.js
@@ -23,62 +23,61 @@ const getImages = (value) => {
     })
     .then((data) => {
       // creo un array a partire dai dati ricevuti in risposta
-      const imagesArray = [];
-      imagesArray.push(...data.photos);
 
-      // manipolo il DOM
-
-      // seleziono il
-      const wrapper = document.querySelector(".album .row");
-      wrapper.classList.add("g-3");
-
-      // ciclo le immagini
-      imagesArray.forEach((image, i) => {
-        // seleziono le card
-        const cards = document.querySelectorAll(".card");
-
-        // gestisco gli stili della card
-        cards[i].classList.add("h-100");
-        cards[i].querySelector(".card-body").classList.add("d-flex", "flex-column", "justify-content-between");
-
-        // accedo all'immagine e modifico
-        const cardImg = cards[i].querySelector("img");
-        cardImg.style.height = "350px";
-        cardImg.style.objectFit = "cover";
-        cardImg.style.cursor = "pointer";
-        cardImg.src = image.src.medium;
-        cardImg.onclick = () => {
-          location.href = `./details.html?id=${image.id}&query=${value}`;
-        };
-        // accedo al titolo e modifico
-        const title = cards[i].querySelector("h5");
-        title.classList.add("mb-3");
-        title.style.cursor = "pointer";
-        title.onclick = () => {
-          location.href = `./details.html?id=${image.id}&query=${value}`;
-        };
-        title.innerText = image.alt;
-        // modifico il testo del secondo bottone della card + funzione per eliminare la card
-        const btn = cards[i].querySelector(".btn-group > button:nth-of-type(2)");
-        btn.innerText = "Hide";
-        btn.onclick = () => {
-          cards[i].closest(".col-md-4").remove();
-        };
-
-        // accedo al primo bottone e mostro il modale con l'immagine grande
-        const viewBtns = cards[i].querySelectorAll(".btn-group > button:nth-of-type(1)");
-        viewBtns.forEach((btn) => {
-          btn.onclick = () => {
-            const modal = document.querySelector(".modal-body");
-            modal.innerHTML = `<img class="w-100" src="${image.src.large}" alt="${image.alt}"/>`;
-          };
-        });
-        cards[i].querySelector(".text-muted").innerText = "ID: " + image.id;
-      });
+      // imagesArray.push(...data.photos);
+      generateImages(data.photos, value);
     })
     .catch((err) => {
       console.log(err);
     });
+};
+
+const generateImages = (array, value) => {
+  // seleziono il contenitore delle immagini
+  const wrapper = document.querySelector(".album .row");
+  wrapper.classList.add("g-3");
+
+  // ciclo le immagini
+  array.forEach((image, i) => {
+    const cards = document.querySelectorAll(".card");
+    // seleziono le card
+
+    // gestisco gli stili della card
+    cards[i].classList.add("h-100");
+    cards[i].querySelector(".card-body").classList.add("d-flex", "flex-column", "justify-content-between");
+
+    // accedo all'immagine e modifico
+    const cardImg = cards[i].querySelector("img");
+    cardImg.style.cursor = "pointer";
+    cardImg.src = image.src.portrait;
+    cardImg.onclick = () => {
+      location.href = `./details.html?id=${image.id}&query=${value}`;
+    };
+    // accedo al titolo e modifico
+    const title = cards[i].querySelector("h5");
+    title.classList.add("mb-3");
+    title.style.cursor = "pointer";
+    title.onclick = () => {
+      location.href = `./details.html?id=${image.id}&query=${value}`;
+    };
+    title.innerText = image.alt;
+    // modifico il testo del secondo bottone della card + funzione per eliminare la card
+    const btn = cards[i].querySelector(".btn-group > button:nth-of-type(2)");
+    btn.innerText = "Hide";
+    btn.onclick = () => {
+      cards[i].closest(".col-md-4").remove();
+    };
+
+    // accedo al primo bottone e mostro il modale con l'immagine grande
+    const viewBtns = cards[i].querySelectorAll(".btn-group > button:nth-of-type(1)");
+    viewBtns.forEach((btn) => {
+      btn.onclick = () => {
+        const modal = document.querySelector(".modal-body");
+        modal.innerHTML = `<img class="w-100" src="${image.src.large}" alt="${image.alt}"/>`;
+      };
+    });
+    cards[i].querySelector(".text-muted").innerText = "ID: " + image.id;
+  });
 };
 
 // se l'url contiene il parametro la fetch viene eseguita con quel parametro
