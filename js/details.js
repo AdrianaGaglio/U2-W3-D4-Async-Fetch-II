cae7d2b80fe27476100b9880d1bcae801a341aeb
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-09-26 17:08:27 +0200

    add comments

--- js/details.js
+++ js/details.js
@@ -1,9 +1,12 @@
 const apiKey = "fmpk1y9oq5soBB6RI8HjosKouXXf74ApKIVxBN5lgoHXGoItqFuh65hs";
+
+// prelevo i parametri dalla url quando l'utente torna dalla pagina details
 const addressBarContent = new URLSearchParams(location.search);
 const imageID = addressBarContent.get("id");
 const query = addressBarContent.get("query");
 const url = "https://api.pexels.com/v1/";
 
+// fetch a partire dall'id
 const getImageDetails = () => {
   fetch(url + "photos/" + imageID, {
     headers: { Authorization: apiKey }
@@ -26,19 +29,23 @@ const getImageDetails = () => {
 
 getImageDetails();
 
+// mostro i dettagli dell'immagine
 const showImageDetails = (details) => {
+  // accedo al contenitore
   const imageDetails = document.getElementById("imageDetails");
   imageDetails.classList.add("py-5");
+  // creo colonna per l'immagine
   const imgCol = document.createElement("div");
   imgCol.classList.add("col-8", "mx-auto");
   imgCol.innerHTML = `
   <img class="w-100 mb-3" src="${details.src.large}" alt="${details.alt}"/>`;
+  // creo colonna per i dettagli
   imageDetails.appendChild(imgCol);
   const detailsCol = document.createElement("div");
   detailsCol.classList.add("col-12", "text-center");
   detailsCol.innerHTML = `
   <h1>${details.alt}</h1>
   <h5 class="mb-5">Photographer: <a class="text-decoration-none text-success" href="${details.photographer_url}">${details.photographer}</h5></a>
-  <a class="text-decoration-none text-secondary" href="./?query=${query}">Back to research result</a>`;
+  <a class="text-decoration-none text-secondary" href="./?query=${query}">Back to research result</a>`; // pulsante che ritorna alla pagina di ricerca passando la chiave di ricerca
   imageDetails.appendChild(detailsCol);
 };
--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 const apiKey = "fmpk1y9oq5soBB6RI8HjosKouXXf74ApKIVxBN5lgoHXGoItqFuh65hs";
-
 const url = "https://api.pexels.com/v1/search?query=";
 
+// prelevo i parametri dalla url quando l'utente torna dalla pagina details
 const addressBarContent = new URLSearchParams(location.search);
 const query = addressBarContent.get("query");
 
+// se la url contiene il parametro query, viene usato quel parametro di ricerca
 const value = query ? query : "";
 
+// funzione fetch
 const getImages = (value) => {
   document.querySelector(".album").classList.add("d-block");
   fetch(url + value, {
@@ -20,15 +22,26 @@ const getImages = (value) => {
       }
     })
     .then((data) => {
+      // creo un array a partire dai dati ricevuti in risposta
       const imagesArray = [];
       imagesArray.push(...data.photos);
+
       // manipolo il DOM
+
+      // seleziono il
       const wrapper = document.querySelector(".album .row");
       wrapper.classList.add("g-3");
+
+      // ciclo le immagini
       imagesArray.forEach((image, i) => {
+        // seleziono le card
         const cards = document.querySelectorAll(".card");
+
+        // gestisco gli stili della card
         cards[i].classList.add("h-100");
         cards[i].querySelector(".card-body").classList.add("d-flex", "flex-column", "justify-content-between");
+
+        // accedo all'immagine e modifico
         const cardImg = cards[i].querySelector("img");
         cardImg.style.height = "350px";
         cardImg.style.objectFit = "cover";
@@ -37,6 +50,7 @@ const getImages = (value) => {
         cardImg.onclick = () => {
           location.href = `./details.html?id=${image.id}&query=${value}`;
         };
+        // accedo al titolo e modifico
         const title = cards[i].querySelector("h5");
         title.classList.add("mb-3");
         title.style.cursor = "pointer";
@@ -44,11 +58,14 @@ const getImages = (value) => {
           location.href = `./details.html?id=${image.id}&query=${value}`;
         };
         title.innerText = image.alt;
+        // modifico il testo del secondo bottone della card + funzione per eliminare la card
         const btn = cards[i].querySelector(".btn-group > button:nth-of-type(2)");
         btn.innerText = "Hide";
         btn.onclick = () => {
           cards[i].closest(".col-md-4").remove();
         };
+
+        // accedo al primo bottone e mostro il modale con l'immagine grande
         const viewBtns = cards[i].querySelectorAll(".btn-group > button:nth-of-type(1)");
         viewBtns.forEach((btn) => {
           btn.onclick = () => {
@@ -64,20 +81,24 @@ const getImages = (value) => {
     });
 };
 
+// se l'url contiene il parametro la fetch viene eseguita con quel parametro
 if (query) {
   getImages(query);
 }
 
+// fetch con parametro default "hamsters"
 const primaryBtn = document.querySelector(".btn-primary");
 primaryBtn.onclick = (event) => {
   getImages("hamsters");
 };
 
+// fetch con parametro default "tigers"
 const secondaryBtn = document.querySelector(".btn-secondary");
 secondaryBtn.onclick = () => {
   getImages("tigers");
 };
 
+// fetch con chiave di ricerca custom
 const customSearch = document.querySelector("form");
 customSearch.onsubmit = (e) => {
   e.preventDefault();
